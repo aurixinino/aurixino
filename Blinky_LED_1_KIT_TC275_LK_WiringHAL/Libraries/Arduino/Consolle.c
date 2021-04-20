050f5eb40272fc0f8cb3ce348e1da3787a9bb426
Gino Latino <82281083+aurixinino@users.noreply.github.com>
2021-04-20 10:13:00 +0200

    Update Consolle.c
    fix for non working Consolle_print() and Consolle_println()

--- Blinky_LED_1_KIT_TC275_LK_WiringHAL/Libraries/Arduino/Consolle.c
+++ Blinky_LED_1_KIT_TC275_LK_WiringHAL/Libraries/Arduino/Consolle.c
@@ -33,9 +33,11 @@
 
 #include "Arduino.h"
 
+#include "string.h"
 #include "Ifx_Fifo.h"
 #include "IfxAsclin_Asc.h"
 #include "IfxCpu_Irq.h"
+
 
 //****************************************************************************
 // @Macros
@@ -45,18 +47,14 @@
 //****************************************************************************
 // @Defines
 //****************************************************************************
-#define UART_BAUDRATE           115200                                  /* UART baud rate in bit/s                  */
+/* Communication parameters */
+#define ISR_PRIORITY_ASCLIN_TX      8                                       /* Priority for interrupt ISR Transmit  */
+#define ISR_PRIORITY_ASCLIN_RX      4                                       /* Priority for interrupt ISR Receive   */
+#define ISR_PRIORITY_ASCLIN_ER      12                                      /* Priority for interrupt ISR Errors    */
+#define ASC_TX_BUFFER_SIZE          256                                     /* Define the TX buffer size in byte    */
+#define ASC_RX_BUFFER_SIZE          256                                     /* Define the RX buffer size in byte    */
+#define ASC_BAUDRATE                115200                                  /* Define the UART baud rate            */
 
-#define UART_PIN_RX             IfxAsclin0_RXA_P14_1_IN                 /* UART receive port pin                    */
-#define UART_PIN_TX             IfxAsclin0_TX_P14_0_OUT                 /* UART transmit port pin                   */
-
-/* Definition of the interrupt priorities */
-#define INTPRIO_ASCLIN1_RX      18
-#define INTPRIO_ASCLIN1_TX      19
-
-#define UART_RX_BUFFER_SIZE     64                                      /* Definition of the receive buffer size    */
-#define UART_TX_BUFFER_SIZE     64                                      /* Definition of the transmit buffer size   */                     */
-#define SIZE                    13                                      /* Size of the string                       */
 
 //****************************************************************************
 // @Typedefs
@@ -71,24 +69,15 @@
 //****************************************************************************
 // @Global Variables
 //****************************************************************************
-/* Declaration of the ASC handle */
-static IfxAsclin_Asc g_ascHandle;
+IfxStdIf_DPipe  g_ascStandardInterface;                                     /* Standard interface object            */
+IfxAsclin_Asc   g_asclin;                                                   /* ASCLIN module object                 */
 
-/* Declaration of the FIFOs parameters */
-#if 0
-static uint8 g_ascRxBuffer[UART_RX_BUFFER_SIZE + sizeof(Ifx_Fifo) + 8];
-static uint8 g_ascTxBuffer[UART_TX_BUFFER_SIZE + sizeof(Ifx_Fifo) + 8];
-#else
-static uint8 g_ascRxBuffer[100];
-static uint8 g_ascTxBuffer[100];
-#endif
-
-/* Definition of txData and rxData */
-uint8 g_txData[] = "Hello World!";
-uint8 g_rxData[SIZE] = {''};
-
-/* Size of the message */
-Ifx_SizeT g_count = sizeof(g_txData);
+/* The transfer buffers allocate memory for the data itself and for FIFO runtime variables.
+ * 8 more bytes have to be added to ensure a proper circular buffer handling independent from
+ * the address to which the buffers have been located.
+ */
+uint8 g_uartTxBuffer[ASC_TX_BUFFER_SIZE + sizeof(Ifx_Fifo) + 8];
+uint8 g_uartRxBuffer[ASC_RX_BUFFER_SIZE + sizeof(Ifx_Fifo) + 8];
 
 
 //****************************************************************************
@@ -102,17 +91,36 @@ Ifx_SizeT g_count = sizeof(g_txData);
 /*********************************************************************************************************************/
 /*---------------------------------------------Function Implementations----------------------------------------------*/
 /*********************************************************************************************************************/
-/* Adding of the interrupt service routines */
-IFX_INTERRUPT(asclin1TxISR, 0, INTPRIO_ASCLIN1_TX);
-void asclin1TxISR(void)
+/* Macro to define Interrupt Service Routine.
+ * This macro makes following definitions:
+ * 1) Define linker section as .intvec_tc<vector number>_<interrupt priority>.
+ * 2) define compiler specific attribute for the interrupt functions.
+ * 3) define the Interrupt service routine as ISR function.
+ *
+ * IFX_INTERRUPT(isr, vectabNum, priority)
+ *  - isr: Name of the ISR function.
+ *  - vectabNum: Vector table number.
+ *  - priority: Interrupt priority. Refer Usage of Interrupt Macro for more details.
+ */
+IFX_INTERRUPT(asc0TxISR, 0, ISR_PRIORITY_ASCLIN_TX);
+
+void asc0TxISR(void)
 {
-    IfxAsclin_Asc_isrTransmit(&g_ascHandle);
+    IfxStdIf_DPipe_onTransmit(&g_ascStandardInterface);
 }
 
-IFX_INTERRUPT(asclin1RxISR, 0, INTPRIO_ASCLIN1_RX);
-void asclin1RxISR(void)
+IFX_INTERRUPT(asc0RxISR, 0, ISR_PRIORITY_ASCLIN_RX);
+
+void asc0RxISR(void)
 {
-    IfxAsclin_Asc_isrReceive(&g_ascHandle);
+    IfxStdIf_DPipe_onReceive(&g_ascStandardInterface);
+}
+
+IFX_INTERRUPT(asc0ErrISR, 0, ISR_PRIORITY_ASCLIN_ER);
+
+void asc0ErrISR(void)
+{
+    IfxStdIf_DPipe_onError(&g_ascStandardInterface);
 }
 
 //****************************************************************************
@@ -136,36 +144,50 @@ void asclin1RxISR(void)
 //****************************************************************************
 void Consolle_init(void)
 {
-    /* Initialize an instance of IfxAsclin_Asc_Config with default values */
-    IfxAsclin_Asc_Config ascConfig;
-    IfxAsclin_Asc_initModuleConfig(&ascConfig, &MODULE_ASCLIN1);
+    IfxAsclin_Asc_Config ascConf;
+
+    /* Set default configurations */
+    IfxAsclin_Asc_initModuleConfig(&ascConf, &MODULE_ASCLIN0); /* Initialize the structure with default values      */
 
     /* Set the desired baud rate */
-    ascConfig.baudrate.baudrate = UART_BAUDRATE;
+    ascConf.baudrate.baudrate = ASC_BAUDRATE;                                   /* Set the baud rate in bit/s       */
+    ascConf.baudrate.oversampling = IfxAsclin_OversamplingFactor_16;            /* Set the oversampling factor      */
+
+    /* Configure the sampling mode */
+    ascConf.bitTiming.medianFilter = IfxAsclin_SamplesPerBit_three;             /* Set the number of samples per bit*/
+    ascConf.bitTiming.samplePointPosition = IfxAsclin_SamplePointPosition_8;    /* Set the first sample position    */
 
     /* ISR priorities and interrupt target */
-    ascConfig.interrupt.txPriority = INTPRIO_ASCLIN1_TX;
-    ascConfig.interrupt.rxPriority = INTPRIO_ASCLIN1_RX;
-    ascConfig.interrupt.typeOfService = IfxCpu_Irq_getTos(IfxCpu_getCoreIndex());
-
-    /* FIFO configuration */
-    ascConfig.txBuffer = &g_ascTxBuffer;
-    //TODO: ascConfig.txBufferSize = UART_TX_BUFFER_SIZE;
-    ascConfig.rxBuffer = &g_ascRxBuffer;
-    //TODO: ascConfig.rxBufferSize = UART_RX_BUFFER_SIZE;
+    ascConf.interrupt.txPriority = ISR_PRIORITY_ASCLIN_TX;  /* Set the interrupt priority for TX events             */
+    ascConf.interrupt.rxPriority = ISR_PRIORITY_ASCLIN_RX;  /* Set the interrupt priority for RX events             */
+    ascConf.interrupt.erPriority = ISR_PRIORITY_ASCLIN_ER;  /* Set the interrupt priority for Error events          */
+    ascConf.interrupt.typeOfService = IfxSrc_Tos_cpu0;
 
     /* Pin configuration */
-    const IfxAsclin_Asc_Pins pins =
-    {
-        NULL_PTR,       IfxPort_InputMode_pullUp,     /* CTS pin not used */
-        &UART_PIN_RX,   IfxPort_InputMode_pullUp,     /* RX pin           */
-        NULL_PTR,       IfxPort_OutputMode_pushPull,  /* RTS pin not used */
-        &UART_PIN_TX,   IfxPort_OutputMode_pushPull,  /* TX pin           */
-        IfxPort_PadDriver_cmosAutomotiveSpeed1
+    const IfxAsclin_Asc_Pins pins = {
+            .cts        = NULL_PTR,                         /* CTS pin not used                                     */
+            .ctsMode    = IfxPort_InputMode_pullUp,
+            .rx         = &IfxAsclin0_RXA_P14_1_IN,         /* Select the pin for RX connected to the USB port      */
+            .rxMode     = IfxPort_InputMode_pullUp,         /* RX pin                                               */
+            .rts        = NULL_PTR,                         /* RTS pin not used                                     */
+            .rtsMode    = IfxPort_OutputMode_pushPull,
+            .tx         = &IfxAsclin0_TX_P14_0_OUT,         /* Select the pin for TX connected to the USB port      */
+            .txMode     = IfxPort_OutputMode_pushPull,      /* TX pin                                               */
+            .pinDriver  = IfxPort_PadDriver_cmosAutomotiveSpeed1
     };
-    ascConfig.pins = &pins;
+    ascConf.pins = &pins;
 
-    IfxAsclin_Asc_initModule(&g_ascHandle, &ascConfig); /* Initialize module with above parameters */
+    /* FIFO buffers configuration */
+    ascConf.txBuffer = g_uartTxBuffer;                      /* Set the transmission buffer                          */
+    ascConf.txBufferSize = ASC_TX_BUFFER_SIZE;              /* Set the transmission buffer size                     */
+    ascConf.rxBuffer = g_uartRxBuffer;                      /* Set the receiving buffer                             */
+    ascConf.rxBufferSize = ASC_RX_BUFFER_SIZE;              /* Set the receiving buffer size                        */
+
+    /* Init ASCLIN module */
+    IfxAsclin_Asc_initModule(&g_asclin, &ascConf);          /* Initialize the module with the given configuration   */
+
+    /* Initialize the Standard Interface */
+    IfxAsclin_Asc_stdIfDPipeInit(&g_ascStandardInterface, &g_asclin);
 }
 
 
@@ -183,7 +205,7 @@ void Consolle_init(void)
 // @Returnvalue   None
 //
 //----------------------------------------------------------------------------
-// @Parameters    None
+// @Parameters    char      message[STDIF_DPIPE_MAX_PRINT_SIZE + 1];
 //
 //----------------------------------------------------------------------------
 // @Link          https://www.infineon.com/dgdl/Infineon-AURIX_ASCLIN_UART_1_KIT_TC297_TFT-Training-v01_01-EN.pdf?fileId=5546d462766cbe8601768b8b1bfa38af
@@ -191,20 +213,43 @@ void Consolle_init(void)
 // @Date          15/04/2021
 //
 //****************************************************************************
-void Consolle_print(const void *data)
+void Consolle_print(const void *message)
 {
-    IfxAsclin_Asc_write(&g_ascHandle, g_txData, &g_count, TIME_INFINITE);
+    Ifx_SizeT count = (Ifx_SizeT)strlen(message);
+    IfxStdIf_DPipe_write(&g_ascStandardInterface, (void *)message, &count, TIME_INFINITE);
 }
 
 
-
-/* This function sends and receives the string "Hello World!" */
-void send_receive_ASCLIN_UART_message(void)
+//****************************************************************************
+// @Function     Consolle_println
+//
+//----------------------------------------------------------------------------
+// @Description   The print() method writes data to the serial port. This is
+//                  often helpful for looking at the data a program is
+//                  producing or to write data to other devices connected to
+//                  the serial port. The println method works like print, but
+//                  sends a new line character for each call to the function.
+//
+//
+//----------------------------------------------------------------------------
+// @Returnvalue   None
+//
+//----------------------------------------------------------------------------
+// @Parameters    char      message[STDIF_DPIPE_MAX_PRINT_SIZE + 1];
+//
+//----------------------------------------------------------------------------
+// @Link          https://www.infineon.com/dgdl/Infineon-AURIX_ASCLIN_UART_1_KIT_TC297_TFT-Training-v01_01-EN.pdf?fileId=5546d462766cbe8601768b8b1bfa38af
+//----------------------------------------------------------------------------
+// @Date          15/04/2021
+//
+//****************************************************************************
+void Consolle_println(const void *message)
 {
-    IfxAsclin_Asc_write(&g_ascHandle, g_txData, &g_count, TIME_INFINITE);   /* Transmit data via TX */
-    IfxAsclin_Asc_read(&g_ascHandle, g_rxData, &g_count, TIME_INFINITE);    /* Receive data via RX  */
+    // Conactenate given string with CR-NL and print
+    char      msg[STDIF_DPIPE_MAX_PRINT_SIZE + 1];
+    strcat ( (char * ) message, ENDLINE);
+    Consolle_print(msg);
 }
-
 
 
 
